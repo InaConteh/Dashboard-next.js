e36a8a492d789bd42cbc99fcab17cdb2035d5581
InaConteh <inaconteh00@gmail.com>
2025-09-25 20:34:58 +0000

    updated code

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -4,6 +4,13 @@ import { redirect } from 'next/navigation';
 import postgres from 'postgres';
  
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+const FormSchema = z.object({
+  id: z.string(),
+  date: z.date(),
+  customerId: z.string(),
+  amount: z.number(),
+  status: z.enum(["pending", "paid", "cancelled"]),
+});
 // Use Zod to update the expected types
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
  
@@ -26,4 +33,9 @@ export async function updateInvoice(id: string, formData: FormData) {
  
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath('/dashboard/invoices');
 }
