b4ddef4e14807e6c7f82d84281f92e70bb51a4f4
InaConteh <inaconteh00@gmail.com>
2025-09-25 20:18:05 +0000

    updated code

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -4,23 +4,26 @@ import { redirect } from 'next/navigation';
 import postgres from 'postgres';
  
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+// Use Zod to update the expected types
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
  
-
+// ...
  
-export async function createInvoice(formData: FormData) {
-  const { customerId, amount, status } = CreateInvoice.parse({
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
+ 
   const amountInCents = amount * 100;
-  const date = new Date().toISOString().split('T')[0];
  
   await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
   `;
-
-   revalidatePath('/dashboard/invoices');
-   redirect('/dashboard/invoices');
+ 
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
 }
